a4c0f0d25b67504cecf30fcaee4f6a2f2cb5e4b7
Maggie Summers and Nicko Dela Cruz <nicko@>
2018-01-08 16:41:54 -0800

    add third function and concat

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,29 @@ var sentence = prompt("Enter a sentence:");
 var length = sentence.length;
 
 var firstLastToCapital = function(sent) {
-	return sent.charAt(0).toUpperCase() + sent.charAt(length-1).toUpperCase();
+  return sent.charAt(0).toUpperCase() + sent.charAt(length-1).toUpperCase();
 }
 
-alert(firstLastToCapital(sentence))
+alert(firstLastToCapital(sentence));
 
 var reverseOrder = function(sent) {
-	return sent.charAt(length-1).toUpperCase() +
-sent.charAt(0).toUpperCase();
+  return sent.charAt(length-1).toUpperCase() +
+  sent.charAt(0).toUpperCase();
 }
 
-alert(reverseOrder(sentence))
+alert(reverseOrder(sentence));
+
+var thirdFunction = function(sent) {
+  return sent.charAt(0).toUpperCase() + sent.charAt(length-1).toUpperCase();
+}
+
+alert(thirdFunction(sentence));
+
+var thirdFunction = function(sent) {
+  return sent.charAt(length-1).toUpperCase() +
+  sent.charAt(0).toUpperCase();
+}
+
+alert(thirdFunction(sentence));
+
+alert(sentence.concat("OH"));
